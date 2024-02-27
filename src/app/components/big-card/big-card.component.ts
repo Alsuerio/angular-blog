bc1a5cf5c58a52a7c5d2783954e2f1d7a48f0ec2
Alsuerio <alsuerio72@gmail.com>
2024-02-27 18:00:03 -0300

    Novos incrementos  no projeto

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -7,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BigCardComponent implements OnInit {
 
+  @Input()
+photoCover: string=""
+@Input()
+cardTitle: string=""
+@Input()
+cardDescription: string=""
   constructor() { }
-
+ 
   ngOnInit(): void {
   }
 
