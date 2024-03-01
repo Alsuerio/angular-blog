787a616934c1443c383388eafb9a349d2f5ff8c7
Alsuerio <alsuerio72@gmail.com>
2024-03-01 18:21:00 -0300

    content created

--- src/app/Data/datafake.ts
+++ src/app/Data/datafake.ts
@@ -1,13 +1,13 @@
 export const dataFake = [
    {"id" : "1",
     "title" : "NOVA ATUALIZAÇÃO",
-    "descripition" : "Aprecie nova atualização do angular",
+    "description" : "Aprecie nova atualização do angular",
     "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgXyguxiL7XTg7BiQhGXFv_rS2HEyKkLrMuscU1GiFS00v8Hp4xLRQdrovrVsNLYNMcww&usqp=CAU"
    },
 
    {"id" : "2",
     "title" : "Angula facilita seu desenvolvimento",
-    "descripition" : "Siga",
+    "description" : "Bem-vindo ao  Angular!",
     "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCkmZ614Q4pKBgCXAoTIJVgu4b5BB6mGrbbF4bk9h5_1BX_Zk7ImmJ6Y5JXX6wiZsgZqI&usqp=CAU"
     
    }
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -8,13 +8,11 @@ import {dataFake} from '../../Data/datafake'
 })
 export class ContentComponent implements OnInit {
     
-  //photoCover:string ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgXyguxiL7XTg7BiQhGXFv_rS2HEyKkLrMuscU1GiFS00v8Hp4xLRQdrovrVsNLYNMcww&usqp=CAU" 
- // photoCover:string = "https://miro.medium.com/v2/resize:fit:1400/1*qgo4_MwETtxdspEqt12sNA.png"
-  //contentTitle:string = "NOVA ATUALIZAÇAO"
+  
   photoCover:string =""
   contentTitle:string =""
   contentDescription:string=""
-  //contentDescription:string = "Veja novas atualizaçoes"
+  
   private id:string | null ="0"
 
 
@@ -32,7 +30,7 @@ setValuesToComponent(id:string | null){
   const result = dataFake.filter(article => article.id==id)[0]
 
   this.contentTitle = result.title
-  this.contentDescription = result.descripition
+  this.contentDescription = result.description
   this.photoCover = result.photoCover
 
 }
